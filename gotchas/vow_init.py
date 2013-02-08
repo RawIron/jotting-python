a9bda0694407ad9a46f65321e45aed6c364dd712
RawIron <michael_nathe@hotmail.com>
2013-02-08 09:47:21 -0800

    fix the vow for init

--- gotchas/vow_init.py
+++ gotchas/vow_init.py
@@ -1,15 +1,16 @@
 
 from pyvows import Vows, expect
 
-
+@Vows.batch
 class InitPy(Vows.Context):
     class InThePackageRootUsingImport():
         def topic(self):
             import gotchas
+            return gotchas
 
         def it_should_(self, topic):
-            expect(topic.name).to_not_exist()
+            expect(topic.name).to_be_an_error_like(AttributeError)
 
         def it_should_show_private_identifiers(self, topic):
-            expect(topic._name).to_not_exist()
+            expect(topic._name).to_be_an_error_like(AttributeError)
 
