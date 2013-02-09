233b62757d83452a2f62d421e65a40d92b86b17e
RawIron <michael_nathe@hotmail.com>
2013-02-09 10:48:03 -0800

    vow for command

--- designPatterns/vow_command.py
+++ designPatterns/vow_command.py
@@ -6,23 +6,34 @@ import command as c
 @Vows.batch
 class ForGuns(Vows.Context):
 
-    class AGunWithOneBullet(Vows.Context):
-
+    class AGun(Vows.Context):
         def topic(self):
             gun = c.Gun()
-            gun.load_the(1)
             return gun
+        def should_not_be_loaded(self, topic):
+            expect(topic.is_loaded()).to_be_false()
 
-        def should_fire_only_once(self, topic):
-            expect(topic.fire_it().is_loaded()).to_be_false()
-
-
-    class AGunWithTwoBullet(Vows.Context):
-
+    class AGunWithBullets(Vows.Context):
         def topic(self):
             gun = c.Gun()
             gun.load_the(2)
             return gun
+        def should_be_loaded(self, topic):
+            expect(topic.is_loaded()).to_be_true()
 
+    class AGunWithOneBullet(Vows.Context):
+        def topic(self):
+            gun = c.Gun()
+            gun.load_the(1)
+            return gun
+        def should_fire_only_once(self, topic):
+            expect(topic.fire_it().is_loaded()).to_be_false()
+
+    class AGunWithTwoBullet(Vows.Context):
+        def topic(self):
+            gun = c.Gun()
+            gun.load_the(2)
+            return gun
         def should_fire_twice(self, topic):
             expect(topic.fire_it().fire_it().is_loaded()).to_be_false()
+
