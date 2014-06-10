1aee661a797c0f7951586d9770f52831fc33c36d
RawIron <manianology@gmail.com>
2014-06-10 11:30:49 -0700

    more attributes

--- mini_game/players/player.py
+++ mini_game/players/player.py
@@ -1,10 +1,55 @@
-
 import datastore.crud as ds
 
 
 class Player(object):
+    id = 0
+    name = ""
+    email = ""
+    password = ""
+    createdAt = 
+    lastPlayedAt =
+    totalSessions = 0
+    totalSessionTime = 0
+    totalActions = 0
+
+    audioToggle = False
+    audioVolume = 0
+    musicToggle = False
+    language = "EN"
+
+    level = 0
+    xp = 0
+    premium = 0
+    totalPremiumBought = 0
+    totalPremiumEarned = 0
+    totalPremiumOut = 0
+    coins = 0
+    totalCoinsBought = 0
+    totalCoinsEarned = 0
+    totalCoinsOut = 0
+
     def __init__(self, ds):
         self.ds = ds
 
     def save(self):
         return self.ds.save(self)
+
+
+class FacebookUser(object):
+    playerId = 0
+    id = 0
+    name = ""
+    email = ""
+    friends = []
+    country = ""
+    gender = ""
+    age = 0
+
+
+class Avatar(object):
+    playerId = 0
+    id = 0
+    hairstyleId = 0
+    clothingId = 0
+    skintone = 0
+
