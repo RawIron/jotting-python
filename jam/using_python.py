6fad1e0217af63a4b0ea7656aede83b5d33e6e0b
RawIron <manianology@gmail.com>
2017-06-04 17:13:41 +0200

    add idiom and different implmentations for count_value in list

--- jam/using_python.py
+++ jam/using_python.py
@@ -3,12 +3,32 @@ http://usingpython.com/python-programming-challenges/
 '''
 
 
-def _reverse_string(str):
+def reverse_string(str):
     return str[::-1]
 
 
-def _copy_list(x):
+def copy_list(x):
     return list(x)
+
+
+def count_value(x, value):
+    return x.count(value)
+
+
+def count_value_comprehension(x, value):
+    return len([n for n in x if n == value])
+
+
+def count_value_functional(x, value):
+    return len(filter(lambda y: y == value, x))
+
+
+def count_value_iteration(x, value):
+    count = 0
+    for n in x:
+        if n == value:
+            count += 1
+    return count
 
 
 def print_banner(headline, marker='*', filler='.:: '):
