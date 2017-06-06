cb3242ca183bcce2ea83a83d2d123460109107f0
RawIron <manianology@gmail.com>
2017-06-06 12:55:48 +0200

    solve using string functions only

--- jam/path_cd.py
+++ jam/path_cd.py
@@ -20,6 +20,59 @@ should display '/a/b/c/x'.
 """
 
 
+class PathString:
+    ''' solve challenge with string operations only '''
+    SEPARATOR = '/'
+
+    @staticmethod
+    def _is_absolute_path(path_str):
+        return path_str.startswith(Path.SEPARATOR)
+
+    @staticmethod
+    def _is_in_subdir(path_str):
+        return path_str != Path.SEPARATOR
+
+    def __init__(self, path):
+        self.current_path = path
+
+    def cd(self, change_path):
+        new_path = self.current_path
+
+        if Path._is_absolute_path(change_path):
+            change_path = change_path[1:]
+            # wipe current path
+            new_path = ""
+
+        # /    + .. = /
+        # /a   + .. = /
+        # /a/b + .. = /a
+        # /a   + b  = /a/b
+        # /a   + /b = /b
+        while change_path:
+            pos = change_path.find(Path.SEPARATOR)
+            if pos > 0:
+                item = change_path[:pos]
+                change_path = change_path[pos+1:]
+            else:
+                item = change_path
+                change_path = ''
+
+            if item == '..':
+                if PathString._is_in_subdir(new_path):
+                    del_pos = new_path.rfind(Path.SEPARATOR)
+                    if del_pos > 0:
+                        new_path = new_path[:del_pos]
+                    elif del_pos == 0:
+                        new_path = new_path[0]
+            else:
+                if new_path.endswith(Path.SEPARATOR):
+                    new_path += item
+                else:
+                    new_path += Path.SEPARATOR + item
+
+        self.current_path = new_path
+
+
 class Path:
     SEPARATOR = '/'
 
