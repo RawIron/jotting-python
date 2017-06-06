a1592a0a4f2b2fff0334450dfa04e7bb2d252dea
RawIron <manianology@gmail.com>
2017-06-06 12:57:24 +0200

    add factory function for Path classes

--- jam/test_path_cd.py
+++ jam/test_path_cd.py
@@ -1,43 +1,47 @@
-from path_cd import Path
+from path_cd import Path, PathString
+
+
+def _create_path():
+    return Path
 
 
 def test_up_down():
-    p = Path('/a/b')
+    p = _create_path()('/a/b')
     p.cd('../x')
     assert p.current_path == '/a/x'
 
 
 def test_absolute():
-    p = Path('/a/b')
+    p = _create_path()('/a/b')
     p.cd('/x/y')
     assert p.current_path == '/x/y'
 
 
 def test_absolute_with_up():
-    p = Path('/a/b')
+    p = _create_path()('/a/b')
     p.cd('/x/../y')
     assert p.current_path == '/y'
 
 
 def test_absolute_with_up_up():
-    p = Path('/a/b')
+    p = _create_path()('/a/b')
     p.cd('/x/../../y')
     assert p.current_path == '/y'
 
 
 def test_up_up_to_root():
-    p = Path('/a/b')
+    p = _create_path()('/a/b')
     p.cd('../..')
     assert p.current_path == '/'
 
 
 def test_up_on_root():
-    p = Path('/')
+    p = _create_path()('/')
     p.cd('..')
     assert p.current_path == '/'
 
 
 def test_up_up_on_root():
-    p = Path('/')
+    p = _create_path()('/')
     p.cd('../..')
     assert p.current_path == '/'
