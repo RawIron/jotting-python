b40c9f2745dc823ca532571eb9c02f734a26a730
RawIron <manianology@gmail.com>
2018-06-06 09:00:36 -0700

    Clean elevator

--- mini_sim/elevator.py
+++ mini_sim/elevator.py
@@ -27,24 +27,44 @@ def test_down_down_ground():
     assert moves == [0, -1, -2, -1, 0]
 
 
+def up(position):
+    return position + 1
+
+def down(position):
+    return position - 1
+
 def move(position, target):
-    if position < target: return position + 1
-    elif position > target: return position - 1
+    if position < target: return up(position)
+    elif position > target: return down(position)
     else: return position
 
+def step_to_target(position, target, requests):
+    return (move(position, target), target, requests)
+
+def pick_next_target(requests):
+    if no_more(requests):
+        return (0, [])
+    else:
+        return (requests[0], requests[1:])
 
 def arrived(position, target):
     return position == target
 
+def no_more(requests):
+    return not requests
+
 
 def serve(position, target, requests):
     if arrived(position, target):
-        if not requests:
-            return (move(position, 0), 0, [])
-        else:
-            return (move(position, requests[0]), requests[0], requests[1:])
-    else:
-        return (move(position, target), target, requests)
+        target, requests = pick_next_target(requests)
+    return step_to_target(position, target, requests)
+
+
+def update(requests, incoming):
+    if incoming:
+        requests += incoming[0]
+        incoming = incoming[1:]
+    return requests, incoming
 
 
 def run(at_floor, to_floor, incoming):
@@ -52,9 +72,7 @@ def run(at_floor, to_floor, incoming):
     target = to_floor
     requests = []
     while not arrived(at_floor, target) or target != 0 or requests:
-        if incoming:
-            requests += incoming[0]
-            incoming = incoming[1:]
+        requests, incoming = update(requests, incoming)
         at_floor, target, requests = serve(at_floor, target, requests)
         moves.append(at_floor)
     return moves
