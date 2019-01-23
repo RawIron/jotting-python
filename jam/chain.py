83edf5335df8d76b570daa28067d65c2cfe059fa
RawIron <manianology@gmail.com>
2019-01-22 19:23:53 -0800

    Add challenge longest path

---
+++ jam/chain.py
@@ -0,0 +1,25 @@
+"""
+given a list of pairs
+find the longest chain of pairs
+a pair can be used exactly once
+
+graph theory:
+    a pair models an edge
+    find the longest path in the graph
+    travel along a given edge exactly once
+    pass through a given node once or more than once
+
+example:
+[(1,3), (3,4), (5,6)]
+[(1,3), (3,4)]
+
+edge cases:
+(1,1)
+(1,2), (2,1)
+(1,2), (2,3), (2,4)
+(1,2), (3,2), (2,4)
+"""
+
+
+def create_chain(head, pairs):
+    pass
