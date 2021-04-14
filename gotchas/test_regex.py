bb3884c78e6186a715c23615774d8a8869f23e29
RawIron <manianology@gmail.com>
2021-04-14 12:22:06 -0700

    Fix warning from test_regex

--- gotchas/test_regex.py
+++ gotchas/test_regex.py
@@ -3,44 +3,44 @@ import re
 
 def test_words_space_one():
   line = "two one"
-  splitter = re.compile('\\W*')
+  splitter = re.compile(r'\s+')
   assert sorted(splitter.split(line)) == ["one", "two",]
 
 def test_words_space_many():
   line = "two      one"
-  splitter = re.compile('\\W*')
+  splitter = re.compile(r'\s+')
   assert sorted(splitter.split(line)) == ["one", "two",]
 
 def test_words_dash_one():
   line = "two-one"
-  splitter = re.compile('\\W+')
+  splitter = re.compile(r'\W+')
   assert sorted(splitter.split(line)) == ["one", "two",]
 
 def test_words_dash_ends():
   line = "two-one--"
-  splitter = re.compile('\\W+')
+  splitter = re.compile(r'\W+')
   assert sorted(splitter.split(line)) == ["", "one", "two",]
 
 def test_words_dash_special_many():
   line = "two- !one"
-  splitter = re.compile('\\W+')
+  splitter = re.compile(r'\W+')
   assert sorted(splitter.split(line)) == ["one", "two",]
 
 def test_words_dash_for_underscore():
   line = "two-one_three"
-  splitter = re.compile('[^a-zA-Z0-9\-]+')
+  splitter = re.compile(r'[^a-zA-Z0-9\-]+')
   assert sorted(splitter.split(line)) == ["three", "two-one",]
 
 
 def test_words_commas():
   line = "two  , one"
-  splitter = re.compile(' *, *')
+  splitter = re.compile(r' *, *')
   assert sorted(splitter.split(line)) == ["one", "two",]
 
 
 def test_sub_wordbreak():
   line = 'one-\ntwo'
-  replacer = re.compile('-\n')
+  replacer = re.compile(r'-\n')
   assert replacer.sub('-', line) == "one-two"
 
 
