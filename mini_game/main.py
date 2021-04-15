47fb74d4f10b04f01f73125af4d93a42593fafc2
RawIron <manianology@gmail.com>
2021-04-15 11:15:10 -0700

    Fix main in mini_game

--- mini_game/main.py
+++ mini_game/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import setuptools
 import sys
@@ -12,11 +12,11 @@ a = [23, 45,]
 b = "34.454"
 c = "45"
 x = b.split('.')
-print x
+print(x)
 
 simple()
 w.forAll()
 w._ds.store()
 
 dir()
-print sys.path
+print(sys.path)
